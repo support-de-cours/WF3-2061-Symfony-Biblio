d1a7e1dccf017eac41113a97f109c3393991447b
Arnaud BODEL <contact@osw3.net>
2023-05-23 14:34:41 +0200

    -

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -8,18 +8,55 @@ use App\Entity\Book;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class BookType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
         $builder
-            ->add('title')
-            ->add('description')
+            // Title
+            ->add('title', TextType::class, [
+
+                // Label options
+                'label' => "Titre du livre",
+                // 'label_attr' => [
+                //     'class' => "my-awesome-style",
+                // ],
+
+                // Make it required
+                'required' => true,
+                
+                // Field attributes
+                'attr' => [
+                    // 'class' => "my-awesome-style",
+                    'placeholder' => "Saisir le titre du livre"
+                ],
+
+                // Field helper
+                'help' => "Saisir le titre du livre",
+                'help_attr' => [
+                    // 'class' => "my-awesome-style"
+                ],
+
+                // Form constraints
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Le titre est obligatoire"
+                    ])
+                ]
+            ])
+
+            // Description
+            ->add('description', TextareaType::class)
+
+            // Cover
             ->add('cover')
-            // ->add('createAt')
-            // ->add('updateAt')
-            // ->add('isPublished')
+
+            
         ;
     }
 
